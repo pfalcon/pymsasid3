062f8d1bc81977afe7025753980a450ca21f0571
asymmail <asymmail@275bb67a-b269-11de-a98d-d9ea7a772a8c>
2010-11-20 16:16:52 +0000

    Add new file header (new BSD license) Clean up code and remove trailing whitespace Use lists for optimized string concatenation Fix intel syntax by adding ', ' between two operands Reserve 8 characters for the operator

--- syn_intel.py
+++ syn_intel.py
@@ -1,121 +1,131 @@
- # -----------------------------------------------------------------------------
- # syn-intel.pyx
- #
- # author: matthieu.kaczmarek@mines-nancy.org
- # Mainly rewrited from udis86 -- Vivek Mohan <vivek@sig9.com>
- # -----------------------------------------------------------------------------
+# Copyright (c) 2009-2010, matthieu.kaczmarek@mines-nancy.org
+# Rewritten from udis86 -- Vivek Mohan <vivek@sig9.com>
+# All rights reserved.
 
 from operand import P_OSO, P_ASO, P_IMPADDR
 from common import DecodeException
 
 def intel_operand_cast(op):
     """Returns operand casts."""
+
     ret = {
-        8: 'byte ',
-        16: 'word ',
-        32: 'dword ',
-        64: 'qword ',
-        84: 'tword '
+        8 :     'byte ',
+        16 :    'word ',
+        32 :    'dword ',
+        64 :    'qword ',
+        84 :    'tword ',
     }
-    try:
+
+    if op.size in ret:
         return ret[op.size]
-    except KeyError:
-        raise DecodeException('unknown operand size: ' + str(op.size))
+    else:
+        raise KeyError('Unknown operand size: %s' % str(op.size))
+
 
 def intel_operand_syntax(op):
     """Generates assembly output for operands."""
-    #return op.value
-    if op.type == None:
+
+    ret = list()
+
+    if op.type is None:
         return ''
 
-    ret = ''
-
     if op.type == 'OP_REG':
-        ret += op.base
-        return ret
+        return op.base
 
-    if op.cast == 1:
-        ret += intel_operand_cast(op)
-        
+    if op.cast is 1:
+        ret.append(intel_operand_cast(op))
+
     if op.type == 'OP_MEM':
-        op_f = 0
-        ret += '['
+        op_f = False
+        ret.append('[')
 
         if op.seg:
-            ret += op.seg + ':'
+            ret.extend([op.seg, ':'])
 
-        if op.base and op.base != None:
-            ret += op.base
-            op_f = 1
+        if op.base is not None:
+            ret.append(op.base)
+            op_f = True
 
-        if op.index and op.index != None:
+        if op.index is not None:
             if op_f:
-                ret += '+'
-            ret += op.index
-            op_f = 1
+                ret.append('+')
+
+            ret.append(op.index)
+            op_f = True
 
         if op.scale:
-            ret += str(op.scale)
+            ret.append(str(op.scale))
 
         if op.offset in [8, 16, 32, 64]:
-            if(op.lval < 0):
-                ret += '-' + hex(-op.lval)
+            if (op.lval < 0):
+                ret.extend(['-', hex(-op.lval)])
             else:
                 if op_f:
-                    # MK ???
-                    if op.lval == 0.0:
-                        op.lval = 0                    
-                    ret += '+' + hex(op.lval)
+                    if op.lval is 0.0:
+                        op.lval = 0
+                    ret.extend(['+', hex(op.lval)])
                 else:
-                    ret += hex(op.lval)
-        ret += ']'
+                    ret.append(hex(op.lval))
+
+        ret.append(']')
 
     elif op.type == 'OP_IMM':
-        ret += hex(op.lval)
+        ret.append(hex(op.lval))
 
     elif op.type == 'OP_JIMM':
-        val = op.pc + op.lval
-        ret += hex(val)
+        ret.append(hex(op.pc + op.lval))
 
     elif op.type == 'OP_PTR':
-        ret += 'word ' + hex(op.lval.seg) + ':' + hex(op.lval.off)
-    return ret
+        ret.extend(['word ', hex(op.lval.seg), ':', hex(op.lval.off)])
+
+    return ''.join(ret)
+
 
 def intel_syntax(self):
-    """translates to intel syntax"""
-    ret = ''
-    # -- prefixes -- 
+    """Translates to intel syntax."""
 
-    # check if P_OSO prefix is used 
+    ret = list()
+
+    # check if P_OSO prefix is used
     if not P_OSO(self.itab_entry.prefix) and self.pfx.opr:
-        if self.dis_mode == 16: 
-            ret += 'o32 '
+        if self.dis_mode is 16:
+            ret.append('o32 ')
         elif self.dis_mode in [32, 64]:
-            ret += 'o16 '
+            ret.append('o16 ')
 
-    # check if P_ASO prefix was used 
+    # check if P_ASO prefix was used
     if not P_ASO(self.itab_entry.prefix) and self.pfx.adr:
-        if self.dis_mode == 16: 
-            ret += 'a32 '
-        elif self.dis_mode == 32:
-            ret += 'a16 '
-        elif self.dis_mode == 64:
-            ret += 'a32 '
+        if self.dis_mode is 16:
+            ret.append('a32 ')
+        elif self.dis_mode is 32:
+            ret.append('a16 ')
+        elif self.dis_mode is 64:
+            ret.append('a32 ')
 
     if self.pfx.lock:
-        ret += 'lock '
+        ret.append('lock ')
+
     if self.pfx.rep:
-        ret += 'rep '
+        ret.append('rep ')
+
     if self.pfx.repne:
-        ret += 'repne '
+        ret.append('repne ')
+
     if P_IMPADDR(self.itab_entry.prefix) and self.pfx.seg:
-        ret += self.pfx.seg
+        ret.append(self.pfx.seg)
 
-    # print the instruction operator 
-    ret += self.operator + ' '
+    # print the instruction operator
+    ret.append(self.operator.ljust(8, ' '))
+
     if self.branch_dist:
-        ret += self.branch_dist + ' '
-    for op in self.operand:
-        ret += intel_operand_syntax(op) + ' '
+        ret.extend([self.branch_dist, ' '])
 
-    return ret
+    # print the operands
+    operands = list()
+    for op in self.operand:
+        operands.append(intel_operand_syntax(op))
+
+    ret.append(', '.join(operands))
+
+    return ''.join(ret)
