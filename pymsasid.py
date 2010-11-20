fd075a2b767b4a7d5a75cea7b21002c3ee35a0b5
asymmail <asymmail@275bb67a-b269-11de-a98d-d9ea7a772a8c>
2010-11-20 16:33:11 +0000

    Add new file header (new BSD license) Use default exceptions Clean up code and remove trailing whitespace

--- pymsasid.py
+++ pymsasid.py
@@ -1,51 +1,48 @@
-#-----------------------------------------------------------------------------
-# pymasid
-#
-# author: matthieu.kaczmarek@mines-nancy.org
-# Mainly rewrited from udis86 -- Vivek Mohan <vivek@sig9.com>
-# -----------------------------------------------------------------------------
+# Copyright (c) 2009-2010, matthieu.kaczmarek@mines-nancy.org
+# Rewritten from udis86 -- Vivek Mohan <vivek@sig9.com>
+# All rights reserved.
 
-# this is intended, we need every public class in input to be in pymsasid's namespace
 from input import *
-
 from inst import Inst
-from common import DecodeException, VENDOR_INTEL, VENDOR_AMD
 import decode as dec
 import syn_intel as intel
 
+from operand import VENDOR_INTEL, VENDOR_AMD
+
 class Pymsasid:
-    def __init__(self, 
-                 mode = None, 
-                 source = '', 
-                 syntax = intel.intel_syntax,
-                 vendor = VENDOR_INTEL,
-                 hook = BufferHook):
+    def __init__(self, mode=None, source='',syntax=intel.intel_syntax,
+                 vendor=VENDOR_INTEL, hook=BufferHook):
         self.error = 0
         self.vendor = self.set_vendor(vendor)
-        self.input = Input(hook, source) 
+        self.input = Input(hook, source)
         self.entry_point = self.pc = long(self.input.hook.entry_point)
-        if mode == None:
+        self.syntax = syntax
+
+        if mode is None:
             self.dis_mode = self.input.hook.dis_mode
         else:
             self.dis_mode = mode
-        self.syntax = syntax
+
 
     def disassemble(self, add):
         try:
             self.seek(add)
             return self.decode()
-        except DecodeException:
+        except Exception:
             return Inst(self.input)
-            
+
+
     def set_vendor(self, vendor):
         if vendor in [VENDOR_INTEL, VENDOR_AMD]:
             self.vendor = vendor
         else:
-            raise Exception('call to Pymsasid.set_vendor() with unknown vendor:' + str(vendor))
-            
+            raise Exception('Unknown vendor: %s' % str(vendor))
+
+
     def seek(self, add):
         self.input.hook.seek(add)
         self.pc = add
+
 
     def decode(self):
         return dec.decode(self)
